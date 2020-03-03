249d45ee48fd45e48786c55bc067833f76083128
georgetegral <georgetegral@gmail.com>
2020-03-03 06:12:13 -0600

    Changed array for List for Cursos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CoreEscuela.Entidades;
 using static System.Console;
 
@@ -9,11 +10,24 @@ namespace CoreEscuela
         static void Main(string[] args)
         {
             var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad: "Bogota", pais: "Colombia");
-            escuela.Cursos = new Curso[]{
-                new Curso() { Nombre = "101" },
-                new Curso() { Nombre = "201" },
-                new Curso() { Nombre = "301" }
+            escuela.Cursos = new List<Curso>(){
+                new Curso() { Nombre = "101", Jornada = TiposJornada.Mañana },
+                new Curso() { Nombre = "201", Jornada = TiposJornada.Mañana },
+                new Curso() { Nombre = "301", Jornada = TiposJornada.Mañana }
             };
+            escuela.Cursos.Add(new Curso() { Nombre = "102", Jornada = TiposJornada.Tarde });
+            escuela.Cursos.Add(new Curso() { Nombre = "202", Jornada = TiposJornada.Tarde });
+
+            var otrColeccion = new List<Curso>(){
+                new Curso() { Nombre = "401", Jornada = TiposJornada.Mañana },
+                new Curso() { Nombre = "501", Jornada = TiposJornada.Mañana },
+                new Curso() { Nombre = "502", Jornada = TiposJornada.Tarde }
+            };
+            //Eliminar todos los elementos de la colección
+            otrColeccion.Clear();
+
+            escuela.Cursos.AddRange(otrColeccion);
+            
             WriteLine(escuela);
             ImprimirCursosEscuela(escuela);
         }
