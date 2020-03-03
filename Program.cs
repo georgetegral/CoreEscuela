4974de0b8cf8c8d52b702ac03408e09c11b8412f
georgetegral <georgetegral@gmail.com>
2020-03-02 18:41:04 -0600

    Created new class Curso and new enum

--- Program.cs
+++ Program.cs
@@ -9,10 +9,16 @@ namespace CoreEscuela
         {
             //Console.WriteLine("Hello World!");
             var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad:"Bogota", pais:"Colombia");
-            //escuela.Pais = "Colombia";
-            //escuela.Ciudad = "Bogotá";
-            //escuela.TipoEscuela = TiposEscuela.Primaria;
+            var curso1 = new Curso() {Nombre="101"};
+            var curso2 = new Curso() {Nombre="201"};
+            var curso3 = new Curso() {Nombre="301"};
+            //var arregloCursos = new Curso[3];
             Console.WriteLine(escuela);
+            System.Console.WriteLine("============");
+
+            System.Console.WriteLine(curso1.Nombre+", "+curso1.UniqueID);
+            System.Console.WriteLine($"{curso2.Nombre}, {curso1.UniqueID}");
+            System.Console.WriteLine(curso3.Nombre+", "+curso3.UniqueID);
         }
     }
 }
