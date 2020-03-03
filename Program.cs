d6662fac653dfe8ba87638a7ff6f97991c2d185d
georgetegral <georgetegral@gmail.com>
2020-03-03 13:42:14 -0600

    Dictionary test

--- Program.cs
+++ Program.cs
@@ -23,6 +23,22 @@ namespace CoreEscuela
                 out int conteoCursos,
                 traeEvaluaciones:false
             );
+            Dictionary<int, string> diccionario = new Dictionary<int, string>();
+            diccionario[0]="Peckerman";
+            diccionario.Add(10,"Juan");
+            diccionario.Add(23,"Lorem Ipsum");
+            foreach (var keyValPair in diccionario)
+            {
+                WriteLine($"Key: {keyValPair.Key} Valor: {keyValPair.Value}");
+            }
+            Printer.WriteTitle("Acceso a diccionario");
+            WriteLine(diccionario[23]);
+            Printer.WriteTitle("Otro diccionario");
+            var dic = new Dictionary<string, string>();
+            dic["Luna"] = "Cuerpo celeste que gira alrededor de la tierra.";
+            WriteLine(dic["Luna"]);
+            dic["Luna"] = "Protagonista de Soy Luna.";
+            WriteLine(dic["Luna"]);
         }
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
