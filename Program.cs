0fef4aa19a314397ef99cdc33bb10b0f7ca3a02d
georgetegral <georgetegral@gmail.com>
2020-03-03 07:00:53 -0600

    Created Escuela Class

--- Program.cs
+++ Program.cs
@@ -9,33 +9,10 @@ namespace CoreEscuela
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad: "Bogota", pais: "Colombia");
-            escuela.Cursos = new List<Curso>(){
-                new Curso() { Nombre = "101", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "201", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "301", Jornada = TiposJornada.Mañana }
-            };
-            escuela.Cursos.Add(new Curso() { Nombre = "102", Jornada = TiposJornada.Tarde });
-            escuela.Cursos.Add(new Curso() { Nombre = "202", Jornada = TiposJornada.Tarde });
-
-            var otrColeccion = new List<Curso>(){
-                new Curso() { Nombre = "401", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "501", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "502", Jornada = TiposJornada.Tarde }
-            };
-            WriteLine(escuela);
-            escuela.Cursos.AddRange(otrColeccion);
-            ImprimirCursosEscuela(escuela);
-            //Forma 1
-            /*
-            escuela.Cursos.RemoveAll(delegate (Curso cur)
-            {
-                return cur.Nombre == "301";
-            });
-            */
-            //Forma 2 con expresiones Lambda
-            escuela.Cursos.RemoveAll((cur) => cur.Nombre == "301" && cur.Jornada == TiposJornada.Mañana);
-            ImprimirCursosEscuela(escuela);
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+            WriteLine(engine);
+            ImprimirCursosEscuela(engine.Escuela);
         }
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
