9738cd3a40dc712040226b6ba3bb74910524a187
georgetegral <georgetegral@gmail.com>
2020-03-03 12:52:51 -0600

    Implemented region in EscuelaEngine

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -23,25 +23,26 @@ namespace CoreEscuela
             CargarEvaluacionesAlAzar();
         }
 
-        private void CargarEvaluacionesAlAzar()
-        {
-            string[] evaluaciones = { "Tarea 1", "Tarea 2", "Tarea 3", "Proyecto parcial", "Examen parcial" };
-            foreach (var curso in Escuela.Cursos)
-            {
-                foreach (var asignatura in curso.Asignaturas)
-                {
-                    foreach (var alumno in curso.Alumnos)
-                    {
-                        for (int i = 0; i < evaluaciones.Length; i++)
-                        {
-                            var evaluacion = new Evaluacion(evaluaciones[i],alumno,asignatura,ObtenerNotaAleatoria());
-                            alumno.Evaluaciones.Add(evaluacion);
-                        }
-                    }
-                }
-            }
+        
+        private float ObtenerNotaAleatoria(){
+            Random rnd = new Random();
+            float nota = (float) (5* rnd.NextDouble());
+            return nota;
         }
-
+        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
+        {
+            string[] nombre1 = { "Jorge", "Gabriel", "Juan", "Federico", "Elsa", "Patricia", "Daniela" };
+            string[] nombre2 = { "René", "Aldahir", "Lopez", "María", "Patricio" };
+            string[] apellido1 = { "García", "López", "Jiménez", "Rosado", "Uribe", "Garza" };
+            string[] apellido2 = { "Rosado", "Soto", "Reséndiz", "Santana", "Doriga" };
+            //Usando Linq
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               from a2 in apellido2
+                               select new Alumno { Nombre = $"{n1} {n2} {a1} {a2}" };
+            return listaAlumnos.OrderBy((alumno) => alumno.UniqueID).Take(cantidad).ToList();
+        }
         public List<ObjetoEscuelaBase> GetObjetosEscuela()
         {
             var listaObj = new List<ObjetoEscuelaBase>();
@@ -60,12 +61,25 @@ namespace CoreEscuela
             }
             return listaObj;
         }
-        private float ObtenerNotaAleatoria(){
-            Random rnd = new Random();
-            float nota = (float) (5* rnd.NextDouble());
-            return nota;
+        #region Métodos de carga
+        private void CargarEvaluacionesAlAzar()
+        {
+            string[] evaluaciones = { "Tarea 1", "Tarea 2", "Tarea 3", "Proyecto parcial", "Examen parcial" };
+            foreach (var curso in Escuela.Cursos)
+            {
+                foreach (var asignatura in curso.Asignaturas)
+                {
+                    foreach (var alumno in curso.Alumnos)
+                    {
+                        for (int i = 0; i < evaluaciones.Length; i++)
+                        {
+                            var evaluacion = new Evaluacion(evaluaciones[i],alumno,asignatura,ObtenerNotaAleatoria());
+                            alumno.Evaluaciones.Add(evaluacion);
+                        }
+                    }
+                }
+            }
         }
-
         private void CargarAsignaturas()
         {
             foreach (var curso in Escuela.Cursos)
@@ -79,22 +93,6 @@ namespace CoreEscuela
                 };
             }
         }
-
-        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
-        {
-            string[] nombre1 = { "Jorge", "Gabriel", "Juan", "Federico", "Elsa", "Patricia", "Daniela" };
-            string[] nombre2 = { "René", "Aldahir", "Lopez", "María", "Patricio" };
-            string[] apellido1 = { "García", "López", "Jiménez", "Rosado", "Uribe", "Garza" };
-            string[] apellido2 = { "Rosado", "Soto", "Reséndiz", "Santana", "Doriga" };
-            //Usando Linq
-            var listaAlumnos = from n1 in nombre1
-                               from n2 in nombre2
-                               from a1 in apellido1
-                               from a2 in apellido2
-                               select new Alumno { Nombre = $"{n1} {n2} {a1} {a2}" };
-            return listaAlumnos.OrderBy((alumno) => alumno.UniqueID).Take(cantidad).ToList();
-        }
-
         private void CargarCursos()
         {
             Escuela.Cursos = new List<Curso>(){
@@ -113,4 +111,5 @@ namespace CoreEscuela
 
         }
     }
+    #endregion
 }
